5f17d495b3bc4c5aa61a7ba956f5f2186281df10
Bran2332 <brandonchacon1993@outlook.com>
2023-07-20 23:35:51 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,9 +5,9 @@ election_csv = os.path.join("Resources", "election_data.csv")
 
 #Lists
 
-
 candidates = []
-clean_candidates_list = []
+
+vote_counter = {}
 
 total_votes = 0 
 
@@ -15,17 +15,38 @@ with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csv_reader)
 
-     for row in csv_reader:
-
+    for row in csv_reader:
+        #count total amount of votes
         total_votes += 1 
-        
-        if row[2] not in clean_candidates_list:
-            clean_candidates_list.append(row[2])
-        
+        candidate_names = row[2]  
+
+        #looks through row 2 and adds names to empty list, avoiding duplicates
+        if candidate_names not in candidates:
+            candidates.append(candidate_names)
+
+            vote_counter[candidate_names] = 0 
+
+        vote_counter[candidate_names] += 1  
 
 
-                          
-print(clean_candidates_list)
+print(vote_counter)
+                      
+
+
+
+        
+
+        
+        
+        
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote
+
+                        
 
 
 
