c7c8fa9fea65223891da719900da0379c5018699
Bran2332 <brandonchacon1993@outlook.com>
2023-07-23 12:15:15 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,7 +32,6 @@ percentage_of_votes_Charles = candidate_votes["Charles Casper Stockham"] / total
 percentage_of_votes_Diana = candidate_votes["Diana DeGette"] / total_votes * 100
 percentage_of_votes_Raymon = candidate_votes["Raymon Anthony Doane"] / total_votes * 100
 
-
 #The winner of the election based on popular vote
 
 print("Election Results")
@@ -45,10 +44,22 @@ print(candidate_votes)
 print("Charles", percentage_of_votes_Charles, "%")
 print("Diane",percentage_of_votes_Diana, "%")
 print("Raymon",percentage_of_votes_Raymon, "%")
-
+print("Winner is Diana Degette with", candidate_votes["Diana DeGette"], "votes")
 #save the output file path
 output_file = os.path.join("analysis", "PyPoll_Analysis_results.csv")
 
 # open the output file, create the rows with the results and then export a csv file.
 with open(output_file, "w", newline='') as datafile:
     writer = csv.writer(datafile)
+
+    writer.writerow(["Election Results"])
+    writer.writerow(["---------------------------------------"])
+# Total number of votes
+    writer.writerow(["Total number of votes: ", total_votes])
+    writer.writerow(["---------------------------------------"])
+# The total number of votes per candidate and the percentage
+    writer.writerow(["Votes per candidate: ", candidate_votes])
+    writer.writerow(["Charles", (str(percentage_of_votes_Charles)) + "%" + " of votes"])
+    writer.writerow(["Diane", (str(percentage_of_votes_Diana)) + "%" + " of votes"])
+    writer.writerow(["Raymon", (str(percentage_of_votes_Raymon)) + "%" + " of votes"])
+    writer.writerow(["The winner is ", candidates[1]])
